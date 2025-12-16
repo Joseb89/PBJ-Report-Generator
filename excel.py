12a091e9378a819da6ff986c7383aebe6ab31652
Joseb89 <eldarion1989@gmail.com>
2025-12-16 17:07:23 -0600

    Updated CSV reader.

--- excel.py
+++ excel.py
@@ -3,3 +3,10 @@ import pandas
 data_frame = pandas.read_csv("PBJ Report.csv")
 
 user_ids_list = data_frame['User ID']
+
+names = data_frame["Provider"]
+
+def set_job_title_code(job_title):
+    job_titles = {"CNA": 10, "LVN": 9, "Medication Aide": 12}
+
+    return job_titles[job_title]
